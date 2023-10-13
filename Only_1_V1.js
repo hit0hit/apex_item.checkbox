da88828238ade2c0552416920220eb280f4e01bc
Dev_Hit <89605089+hit0hit@users.noreply.github.com>
2023-10-13 11:39:32 -0300

    Update Only_1_V1.js

--- Only_1_V1.js
+++ Only_1_V1.js
@@ -2,3 +2,8 @@ $('input[type=checkbox]').on('change', (e) => {
     const el = $(e.target)
     el.parent('.t-Report-cell').siblings().each((n, e) => $(e).children('input').prop('checked', false))
 })
+//////////////////////////////////
+
+ <div class="CARD_ITEM">
+ <input type="SWITCH" name="f01" value="&ID_CHECKLIST_DETALHES." &ITEM_CHECKED./> <span class="ITEM"> &ITEM. </span>
+ </div>
